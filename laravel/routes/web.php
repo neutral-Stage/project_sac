880ce8059f65e353b20e399bd4c0c61d7cc68a9d
neutral-stage <anirbanroy691@gmail.com>
2019-05-08 22:03:22 -0700

    web

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,16 +15,16 @@
 //     return view('front.index');
 // });
 Route::get('/','FrontController@index')->name('home');
-<<<<<<< HEAD
+
 Route::get('/history','FrontController@history')->name('history');
 Route::get('/mission','FrontController@mission')->name('mission');
 Route::get('/values','FrontController@values')->name('values');
 Route::get('/board','FrontController@board')->name('board');
-=======
+
 Route::get('/contact','ContactController@index')->name('contact');
 Route::get('/products','ProductController@index')->name('product.all');
 Route::get('/advartise','AdvartiseController@index')->name('advartise.all');
->>>>>>> 38968b65531b6270fdfb296e3d2345ff40985471
+
 
 // Route::group(['prefix'=>'home'],function(){
 // 	Route::get('/','FrontController@index')->name('home');
